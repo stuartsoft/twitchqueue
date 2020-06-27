dc1f22c14fabb1883f3920c8f6ae4e7ec6a98b77
Stuart Bowman <stuartsoft2012@gmail.com>
2020-06-26 23:35:53 -0400

    Renaming whitelist to allowlist. Fixing a typo in a comment and replacing the download link for the allow list

--- twitchqueue.py
+++ twitchqueue.py
@@ -6,29 +6,29 @@ import time
 #-----globals-----
 #player = "omxplayer -o hdmi"
 player = "vlc -f"
-ShouldUpdateWhitelist = False
+ShouldUpdateAllowlist = False
 RetryNum = 1 #number of times to retry each stream connection
  
 print ("Starting twitch queue. Press ^C to cancel...")
 time.sleep(3)
  
-if ShouldUpdateWhitelist:
+if ShouldUpdateAllowlist:
         #Delete and check for updated list
-        print ("Refreshing streamer whitelist")
-        os.system('rm -f whitelist.txt')
-        updateURL = "http://bit.ly/1ApKTzE"
-        os.system('wget -q -O whitelist.txt %s' %(updateURL))
+        print ("Refreshing streamer allowlist")
+        os.system('rm -f allowlist.txt')
+        updateURL = "https://raw.githubusercontent.com/stuartsoft/twitchqueue/main/allowlist.txt"
+        os.system('wget -q -O allowlist.txt %s' %(updateURL))
  
-#load the whitelist of stream urls
-with open("whitelist.txt","r") as f:
+#load the allowlist of stream urls
+with open("allowlist.txt","r") as f:
         WhiteList = f.readlines()
 f.close()
  
 if len(WhiteList) == 0:
-        print ("Whitelist is empty. Exiting...")
+        print ("Allowlist is empty. Exiting...")
         exit()
 else:
-        print ("Discovered %d whitelist streamers" % (len(WhiteList)))
+        print ("Discovered %d allowlist streamers" % (len(WhiteList)))
  
 #trim off newline of each element
 for i in range(0,len(WhiteList)):
@@ -38,7 +38,7 @@ CurrentStream = 0 #index of stream currently being displayed
  
 print ("\nLooking for online streamers...")
  
-while True:#continue looping through whitelist of streamers
+while True:#continue looping through allowlist of streamers
         if os.name == 'nt':#Windows
                 os.system('livestreamer --retry-open %d twitch.tv/%s best -p "%s"' %(RetryNum,WhiteList[CurrentStream],player))
         else:#not windows
